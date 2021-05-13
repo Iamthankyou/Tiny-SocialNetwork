c6d0c4342fb7bf19fe86efc79101f2f42f9cc650
dxcanh3103 <dxcanh310320@gmail.com>
2021-05-13 11:32:06 +0700

    be reatime comment canhdx

--- social-network/src/main/java/com/devteam/social_network/controller/MessageController.java
+++ social-network/src/main/java/com/devteam/social_network/controller/MessageController.java
@@ -1,27 +1,35 @@
 package com.devteam.social_network.controller;
 
 
+import com.devteam.social_network.domain.PostComment;
 import com.devteam.social_network.sdi.MessageSdi;
+import com.devteam.social_network.sdi.PostCommentSdi;
 import com.devteam.social_network.sdo.MessageSdo;
+import com.devteam.social_network.service.PostCommentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Date;
 
 @RestController
 public class MessageController {
 
+    @Autowired
+    PostCommentService postCommentService;
     @MessageMapping("/chat")
     @SendTo("/topic/message")
-    public MessageSdo send(MessageSdi messageSdi) throws Exception{
-        String time = new SimpleDateFormat("HH:mm").format(new Date());
-        MessageSdo messageSdo = new MessageSdo();
-        messageSdo.setFrom(messageSdi.getFrom());
-        messageSdo.setText(messageSdi.getText());
-        messageSdo.setTime(time);
-
-        return messageSdo;
+    public PostComment send(PostCommentSdi postCommentSdi) throws Exception{
+        PostComment postComment = new PostComment();
+        postComment.setUserEmail(postCommentSdi.getUserEmail());
+        postComment.setContent(postCommentSdi.getContent());
+        postComment.setPostId(postCommentSdi.getPostId());
+        postComment.setCommentDate(LocalDate.now());
+        postComment.setCommentTime(LocalTime.now());
+        return postCommentService.save(postComment);
     }
 }
--- social-network/src/main/java/com/devteam/social_network/sdo/MessageSdo.java
+++ social-network/src/main/java/com/devteam/social_network/sdo/MessageSdo.java
@@ -2,9 +2,14 @@ package com.devteam.social_network.sdo;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Data
 public class MessageSdo {
     private String from;
     private String text;
-    private String time;
+    private LocalDate date;
+    private LocalTime time;
+    private Long postId;
 }
